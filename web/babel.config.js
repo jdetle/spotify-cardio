ba4bf960c0a0e5facd56a3482724d3daf745e29c
John Detlefs <jdetle@gmail.com>
2020-08-10 19:57:18 -0500

    Initial next config

---
+++ web/babel.config.js
@@ -0,0 +1,20 @@
+module.exports = (api) => {
+  api.cache(true);
+  return {
+    presets: ["next/babel", "@zeit/next-typescript/babel"],
+    plugins: [
+      "@babel/proposal-class-properties",
+      "@babel/proposal-object-rest-spread",
+      [
+        "babel-plugin-styled-components",
+        { ssr: true, displayName: true, preprocess: false },
+      ],
+      [
+        "transform-define",
+        {
+          "process.env.NODE_ENV": process.env.NODE_ENV,
+        },
+      ],
+    ],
+  };
+};
---
+++ web/routes.ts
@@ -0,0 +1,14 @@
+import Router from "nextjs-dynamic-routes";
+
+const router = new Router();
+
+router.add({
+  name: "index",
+  pattern: "/",
+});
+router.add({
+  name: "words",
+  pattern: "/words_list",
+});
+
+module.exports = router;
