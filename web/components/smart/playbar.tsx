64ded45d06e9df54f8383bf78700b2d0154129d5
John Detlefs <jdetle@gmail.com>
2021-02-13 14:21:56 -0600

    FVolume ICON SVG

--- web/components/smart/playbar.tsx
+++ web/components/smart/playbar.tsx
@@ -27,10 +27,32 @@ const IconButton = styled.button`
   }
 `;
 
-const VolumeIcon: React.FC = ({}) => {
-  return (
-    <IconButton className="spoticon-volume-off-16 control-button volume-bar__icon" />
-  );
+const VolumeIcon: React.FC<{ volume: number }> = ({ volume }) => {
+  if (volume === 0) {
+    return (
+      <IconButton className="spoticon-volume-off-16 control-button volume-bar__icon" />
+    );
+  } else if (volume < 60) {
+    return (
+      <IconButton className="volume-bar__icon-button control-button">
+        <svg style={{ fill: "currentcolor" }} height={16} width={16}>
+          <path
+            d={`M0 11.032v-6h2.802l5.198-3v12l-5.198-3H0zm7 1.27v-8.54l-3.929 2.27H1v4h2.071L7 12.302zm4.464-2.314q.401-.925.401-1.956 0-1.032-.4-1.957-.402-.924-1.124-1.623L11 3.69q.873.834 1.369 1.957.496 1.123.496 2.385 0 1.262-.496 2.385-.496 1.123-1.369 1.956l-.659-.762q.722-.698 1.123-1.623z`}
+          />
+        </svg>
+      </IconButton>
+    );
+  } else {
+    return (
+      <IconButton className="volume-bar__icon-button control-button">
+        <svg style={{ fill: "currentcolor" }} height={16} width={16}>
+          <path
+            d={`M12.945 1.379l-.652.763c1.577 1.462 2.57 3.544 2.57 5.858s-.994 4.396-2.57 5.858l.651.763a8.966 8.966 0 00.001-13.242zm-2.272 2.66l-.651.763a4.484 4.484 0 01-.001 6.397l.651.763c1.04-1 1.691-2.404 1.691-3.961s-.65-2.962-1.69-3.962zM0 5v6h2.804L8 14V2L2.804 5H0zm7-1.268v8.536L3.072 10H1V6h2.072L7 3.732z`}
+          />
+        </svg>
+      </IconButton>
+    );
+  }
 };
 
 const VolumeSliderContainer = styled.div`
@@ -49,7 +71,7 @@ const VolumeSlider: React.FC = ({}) => {
   const [volume, setVolume] = useState<number>(defaultVolume);
   return (
     <VolumeSliderContainer>
-      <VolumeIcon />
+      <VolumeIcon volume={volume} />
       <ProgressBar progress={volume} setProgress={setVolume} />
     </VolumeSliderContainer>
   );
